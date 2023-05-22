de9bd3657ff2fd7c6edb4ff34a639994be99500d
Tjaard du Plessis <tjaard123@gmail.com>
2023-05-22 14:02:02 +0200

    Let library call the cluster api endpoint

--- lib/src/request_logger.rs
+++ lib/src/request_logger.rs
@@ -27,12 +27,21 @@ impl Fairing for RequestLogger {
         if let Some(Ok(duration)) = start_time.0.map(|st| st.elapsed()) {
             let ms = duration.as_secs() as i32 * 1000 + duration.subsec_millis() as i32;
 
-            // http_requests::app::set("galactica".to_string()),
-            // http_requests::username::set("tjaard@synthesis.co.za".to_string()),
-            // http_requests::request_method::set(req.method().to_string()),
-            // http_requests::request_uri::set(req.uri().to_string()),
-            // http_requests::request_body::set("galactica".to_string()),
-            // http_requests::response_body::set("galactica".to_string()),
+            let metric = MetricRequest {
+                app: "Test".to_owned(),
+                username: "test@synthesis.co.za".to_owned(),
+                request_time_ms: ms,
+                request_method: req.method().to_string(),
+                request_uri: req.uri().to_string(),
+                request_body: "{}".to_owned(),
+                response_body: "{}".to_owned()
+            };
+        
+            let client = reqwest::Client::new();
+            let res = client.post("request-logger.telemetry.svc.cluster.local")
+                .json(&metric)
+                .send()
+                .await?;
         }
     }
 }
